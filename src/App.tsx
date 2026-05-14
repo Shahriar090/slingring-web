9b1914a84384238a705d4cdec3c6947fbcd26300
Shahriar Hossain <shahriarh655@gmail.com>
2026-05-14 22:25:40 +0600

    refactor: added mystic provider to App.ts file

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import MysticProvider from "./providers/MysticProvider";
 
 const App = () => {
   return (
-    <div >
-      <h1 className="text-2xl font-rune text-eldritch">Welcome, This is app component</h1>
-    </div>
+    <MysticProvider>
+      <div>
+        <h1 className="text-2xl font-rune text-eldritch">
+          Welcome, This is app component
+        </h1>
+      </div>
+    </MysticProvider>
   );
 };
 
